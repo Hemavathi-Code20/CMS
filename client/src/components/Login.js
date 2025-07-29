11701c4a0b85ee2886baf9f2c319a158d000f1da
Hemavathi-Code20 <hemavathisiva05@gmail.com>
2025-07-29 13:12:12 +0530

    commit fixing bug in admin page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://clinic-management-lrw8.onrender.com/api/auth/login",
         credentials,
         {
           headers: {
