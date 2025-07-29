be97464342516ff00d0dcc42ecd5a8ceb172378e
Hemavathi-Code20 <hemavathisiva05@gmail.com>
2025-07-29 15:28:04 +0530

    commit fixing bug in patient register page

--- client/src/components/Patient/pages/Register.js
+++ client/src/components/Patient/pages/Register.js
@@ -25,7 +25,7 @@ const Register = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/patient/patientregister",
+        "https://clinic-management-lrw8.onrender.com/api/patient/patientregister",
         formData
       );
       alert(
